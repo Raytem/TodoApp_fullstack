7b2940113134de1a2f980a01d99d9ccd82902eb5
Daniil <Danik1320@yandex.by>
2023-05-21 15:24:26 +0300

    fixes: netlify redirects

--- client/src/components/UI/loader/Loader.tsx
+++ client/src/components/UI/loader/Loader.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import styles from '../loader/loader.module.css';
 import classNames from 'classnames';
+import { motion } from 'framer-motion';
 
 interface LoaderProps {
   isButtonLoader?: boolean
